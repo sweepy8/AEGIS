1cd2e4aeada987a5ec83ec8b94aa09f66deb7c09
Zachary Martin <zachrmartin818@gmail.com>
2025-06-11 15:36:05 -0700

    Updated IMU driver

--- LiDAR/lib/IMU_driver_test.py
+++ LiDAR/lib/IMU_driver_test.py
@@ -3,18 +3,22 @@ I2C Driver Test Program
 
     Read Gyro and Accelerometer by Interfacing Raspberry Pi with MPU6050 using Python
     http://www.electronicwings.com
+
     
-    
-TODO
-    - Switch from running ring buffer to threshold variation from past to present
-        in order to update value. Should fix the startup delay?
+    Connect SDA to pin 3, SCL to pin 5
     
 '''
-import smbus					#import SMBus module of I2C
-from time import sleep          #import
+
+import smbus	            # SMBus module of I2C
+from time import sleep
 from time import time
 
-#some MPU6050 Registers and their Address
+READ_DELAY_SECONDS  = 0.1	# Seconds between reads
+UPDATE_THRESHOLD    = 0.01  # Difference from new to old val., below which no update occurs
+UPDATE_RATE_SECONDS = 5     # How frequently to update, regardless of threshold
+
+# Some MPU6050 registers and their addresses
+DEVICE_ADDR  = 0x68   # MPU6050 device address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
@@ -27,72 +31,72 @@ GYRO_XOUT_H  = 0x43
 GYRO_YOUT_H  = 0x45
 GYRO_ZOUT_H  = 0x47
 
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Ax, Ay, Az = 0, 0, 0
+Gx, Gy, Gz = 0, 0, 0
 
 def MPU_Init():
-    #write to sample rate register
-    bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)	# Sample rate = Gyroscope Output Rate / 1 + SMPLRT_DIV --> 1kHz
+    # Write to sample rate register
+    bus.write_byte_data(DEVICE_ADDR, SMPLRT_DIV, 7)	 # Sample rate = Gyroscope Output Rate / 1 + SMPLRT_DIV --> 1kHz
     
-    #Write to power management register
-    bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)	#Configures pwr mode and clk source via PWR_MGMT_1 register
+    # Write to power management register
+    bus.write_byte_data(DEVICE_ADDR, PWR_MGMT_1, 1)	 # Configures pwr mode and clk source via PWR_MGMT_1 register
     
-    #Write to Configuration register
-    bus.write_byte_data(Device_Address, CONFIG, 0)		#Frame Sync is off, DLPF off
+    # Write to Configuration register
+    bus.write_byte_data(DEVICE_ADDR, CONFIG, 0)		 # Frame Sync is off, DLPF off
     
-    #Write to Gyro configuration register
-    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24) # Disable self test, set FS_SEL to 3 == 2000 deg/s in GYRO_CONFIG register
+    # Write to Gyro configuration register
+    bus.write_byte_data(DEVICE_ADDR, GYRO_CONFIG, 24) # Disable self test, set FS_SEL to 3 == 2000 deg/s in GYRO_CONFIG register
     
-    #Write to interrupt enable register
-    bus.write_byte_data(Device_Address, INT_ENABLE, 1)	# Configuration complete, begin transmitting IMU data
+    # Write to interrupt enable register
+    bus.write_byte_data(DEVICE_ADDR, INT_ENABLE, 1)	 # Configuration complete, begin transmitting IMU data
 
 def read_raw_data(addr):
-    #Accelero and Gyro value are 16-bit
-    high = bus.read_byte_data(Device_Address, addr)
-    low = bus.read_byte_data(Device_Address, addr+1)
+    # Accelero and Gyro value are 16-bit
+    msb_val = bus.read_byte_data(DEVICE_ADDR, addr)
+    lsb_val = bus.read_byte_data(DEVICE_ADDR, addr+1)
 
-    #concatenate higher and lower value
-    value = ((high << 8) | low)
+    # Concatenate higher and lower value
+    value = (msb_val << 8) | lsb_val
     
-    #to get signed value from mpu6050
+    # Unsigned --> Signed value conversion
     if(value > 32768):
-            value = value - 65536
+        value = value - 65536
+
     return value
 
 
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
+def update_val(threshold : float):
+    pass
+
 
 MPU_Init()
 
 print ("Reading Gyroscope and Accelerometer Data:")
 
-READ_DELAY    = 0.5	# seconds
-UPDATE_GYRO_THRESHOLD = 0.5
-
-Ax, Ay, Az, Gx, Gy, Gz = 0, 0, 0, 0, 0, 0
-
 start_time = time()
 
 while True:
 
-    #Read Accelerometer raw value
+    # Read Accelerometer raw value
     acc_x = read_raw_data(ACCEL_XOUT_H)
     acc_y = read_raw_data(ACCEL_YOUT_H)
     acc_z = read_raw_data(ACCEL_ZOUT_H)
     
-    #Read Gyroscope raw value
+    # Read Gyroscope raw value
     gyro_x = read_raw_data(GYRO_XOUT_H)
     gyro_y = read_raw_data(GYRO_YOUT_H)
     gyro_z = read_raw_data(GYRO_ZOUT_H)
     
-    #Full scale range +/- 250 degree/s as per sensitivity scale factor
-    if time()-start_time < 1.0:
-        Ax = acc_x/16384.0 if abs(acc_x/16384.0 - Ax) > UPDATE_GYRO_THRESHOLD else Ax # 1g in +/- 2g range [-2,2] is 65536 / 4 == 16384
-        Ay = acc_y/16384.0 if abs(acc_y/16384.0 - Ay) > UPDATE_GYRO_THRESHOLD else Ay
-        Az = acc_z/16384.0 if abs(acc_z/16384.0 - Az) > UPDATE_GYRO_THRESHOLD else Az
+    # Full scale range +/- 250 degree/s as per sensitivity scale factor
+    if time() - start_time < UPDATE_RATE_SECONDS:
+        Ax = acc_x/16384.0 if abs(acc_x/16384.0 - Ax) > UPDATE_THRESHOLD else Ax # 1g in +/- 2g range [-2,2] is 65536 / 4 == 16384
+        Ay = acc_y/16384.0 if abs(acc_y/16384.0 - Ay) > UPDATE_THRESHOLD else Ay
+        Az = acc_z/16384.0 if abs(acc_z/16384.0 - Az) > UPDATE_THRESHOLD else Az
         
-        Gx = gyro_x/131.0 if abs(gyro_x/131.0 - Gx) > UPDATE_GYRO_THRESHOLD else Gx
-        Gy = gyro_y/131.0 if abs(gyro_y/131.0 - Gy) > UPDATE_GYRO_THRESHOLD else Gy
-        Gz = gyro_z/131.0 if abs(gyro_z/131.0 - Gz) > UPDATE_GYRO_THRESHOLD else Gz
+        Gx = gyro_x/131.0 if abs(gyro_x/131.0 - Gx) > UPDATE_THRESHOLD else Gx
+        Gy = gyro_y/131.0 if abs(gyro_y/131.0 - Gy) > UPDATE_THRESHOLD else Gy
+        Gz = gyro_z/131.0 if abs(gyro_z/131.0 - Gz) > UPDATE_THRESHOLD else Gz
         
     else:
         Ax = acc_x/16384.0
@@ -105,11 +109,7 @@ while True:
         
         start_time = time()
     
-    print(f"G: (X={Gx:9.4f}, Y={Gy:9.4f}, Z={Gz:9.4f})", end=',    ')
-    print(f"A: (X={Ax:9.4f}, Y={Ay:9.4f}, Z={Az:9.4f})")
+    print(f"G: (X={Gx:8.4f}, Y={Gy:8.4f}, Z={Gz:8.4f})", end=',    ')
+    print(f"A: (X={Ax:8.4f}, Y={Ay:8.4f}, Z={Az:8.4f})")
         
-    sleep(READ_DELAY)
-    
-
-    #print ("Gx=%.4f" %Gx, u'\u00b0'+ "/s", "\tGy=%.4f" %Gy, u'\u00b0'+ "/s", "\tGz=%.4f" %Gz, u'\u00b0'+ "/s", "\tAx=%.4f g" %Ax, "\tAy=%.4f g" %Ay, "\tAz=%.4f g" %Az) 	
-    #sleep(1)
+    sleep(READ_DELAY_SECONDS)
